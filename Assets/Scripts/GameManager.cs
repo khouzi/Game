f3741487d1d9720f20ba4d70b9d6debeac395e92
khouzi <khouzem.boussaid@gmail.com>
2024-05-07 16:01:37 +0100

    added flip animation + some minor bug fixes
    I fixed the bugs that occurred while comparing two cards and smoothed out the operation. Additionally, I added an animation for flipping the cards.

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -9,6 +9,7 @@ public class GameManager : MonoBehaviour
     private string path = "Images/FrontCard";
 
     [SerializeField] private Sprite backCard;
+    [SerializeField] private float flipDuration = 0.3f;
 
     private Sprite[] frontCards;
     public List<Sprite> playableCards = new List<Sprite>();
@@ -25,14 +26,10 @@ public class GameManager : MonoBehaviour
 
     private string firstGuessCard, secondGuessCard;
 
-
-
-
     void Awake()
     {
         frontCards = Resources.LoadAll<Sprite>(path);
     }
-
 
     void Start()
     {
@@ -68,7 +65,6 @@ public class GameManager : MonoBehaviour
         }
     }
 
-
     void AddListeners()
     {
         foreach (Button card in flippedCard)
@@ -83,49 +79,51 @@ public class GameManager : MonoBehaviour
 
         int clickedIndex = int.Parse(name);
 
-        // If the clicked button is already the first guess, return
-        if (clickedIndex == firstGuessIndex)
-            return;
-
         if (!firstGuess)
         {
             firstGuess = true;
             firstGuessIndex = clickedIndex;
             firstGuessCard = playableCards[firstGuessIndex].name;
-            flippedCard[firstGuessIndex].image.sprite = playableCards[firstGuessIndex];
+
+            StartCoroutine(FlipCard(flippedCard[firstGuessIndex], playableCards[firstGuessIndex]));
         }
-        else if (!secondGuess)
+        else if (!secondGuess && clickedIndex != firstGuessIndex)
         {
             secondGuess = true;
             secondGuessIndex = clickedIndex;
             secondGuessCard = playableCards[secondGuessIndex].name;
-            flippedCard[secondGuessIndex].image.sprite = playableCards[secondGuessIndex];
+
+            StartCoroutine(FlipCard(flippedCard[secondGuessIndex], playableCards[secondGuessIndex]));
+
             countGuesses++;
+
             StartCoroutine(CheckCards());
-            firstGuess = secondGuess = false;
         }
     }
 
+
     IEnumerator CheckCards()
     {
-        yield return new WaitForSeconds(1f);
+        yield return new WaitForSeconds(0.5f);
 
-        if (firstGuessCard == secondGuessCard)
+        if (firstGuess && secondGuess)
         {
-            flippedCard[firstGuessIndex].interactable = false;
-            flippedCard[secondGuessIndex].interactable = false;
+            if (firstGuessCard == secondGuessCard && firstGuessIndex != secondGuessIndex)
+            {
+                flippedCard[firstGuessIndex].interactable = false;
+                flippedCard[secondGuessIndex].interactable = false;
+                flippedCard[firstGuessIndex].image.color = new Color(0, 0, 0, 0);
+                flippedCard[secondGuessIndex].image.color = new Color(0, 0, 0, 0);
+                CheckEndgame();
+            }
+            else
+            {
+                StartCoroutine(FlipCard(flippedCard[firstGuessIndex], backCard));
+                StartCoroutine(FlipCard(flippedCard[secondGuessIndex], backCard));
+            }
 
-            flippedCard[firstGuessIndex].image.color = new Color(0, 0, 0, 0);
-            flippedCard[secondGuessIndex].image.color = new Color(0, 0, 0, 0);
-
-            CheckEndgame();
+            firstGuess = secondGuess = false;
         }
-        else
-        {
-            flippedCard[firstGuessIndex].image.sprite = backCard;
-            flippedCard[secondGuessIndex].image.sprite = backCard;
-        }
-
     }
 
     void CheckEndgame()
@@ -135,7 +133,36 @@ public class GameManager : MonoBehaviour
         if (countCorrectGuesses == gameGuesses)
         {
             Debug.Log("Game Finished");
-            Debug.Log("It took you" + countGuesses + "to end");
+            Debug.Log("It took you " + countGuesses + " to end");
+        }
+    }
+
+    IEnumerator FlipCard(Button card, Sprite targetSprite)
+    {
+        float elapsedTime = 0f;
+        Quaternion startRotation = card.transform.rotation;
+        Quaternion endRotation = Quaternion.Euler(0f, 90f, 0f);
+
+        while (elapsedTime < flipDuration)
+        {
+            elapsedTime += Time.deltaTime;
+            float t = Mathf.Clamp01(elapsedTime / flipDuration);
+            card.transform.rotation = Quaternion.Slerp(startRotation, endRotation, t);
+            yield return null;
+        }
+
+        card.image.sprite = targetSprite;
+
+        elapsedTime = 0f;
+        startRotation = card.transform.rotation;
+        endRotation = Quaternion.Euler(0f, 0f, 0f);
+
+        while (elapsedTime < flipDuration)
+        {
+            elapsedTime += Time.deltaTime;
+            float t = Mathf.Clamp01(elapsedTime / flipDuration);
+            card.transform.rotation = Quaternion.Slerp(startRotation, endRotation, t);
+            yield return null;
         }
     }
 
